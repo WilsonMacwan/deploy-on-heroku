b5ee5f7f7cd370325c6116775a3dd55d871333f1
Wilson Macwan <mawcanwillson@gmail.com>
2022-09-03 14:04:57 +0530

    Change Made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  React.useEffect(()=>{
+    console.log("Hellooo......")
+  }, [])
   return (
     <div className="App">
       <h2>First App Deployed on Heroku</h2>
